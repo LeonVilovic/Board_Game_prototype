605285da324a3cb93cd4c77c12537dd887c30a27
Leon <vilovic.leon@gmail.com>
2025-01-28 23:21:50 +0100

    28 01 2025
    removed google ads a builed app

--- Assets/Scripts/PlayerStone.cs
+++ Assets/Scripts/PlayerStone.cs
@@ -5,20 +5,43 @@ public class PlayerStone : MonoBehaviour
 
     DiceRoller TheDiceRoller;
 
+    public Tile StartingTile;
+
+    Tile currentTile;
+
+    Tile[] moveQueue;
+
+    Vector3 targetPosition;
+    Vector3 velocity;
+    float smoothTime = 0.25f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         TheDiceRoller = GameObject.FindFirstObjectByType<DiceRoller>();
+
+        targetPosition = this.transform.position;
     }
 
-    public Tile StartingTile;
-    Tile currentTile;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (this.transform.position != targetPosition)
+        {
+            this.transform.position = Vector3.SmoothDamp(this.transform.position, targetPosition, ref velocity, smoothTime);
+
+        }
     }
+
+    void SetNewTargetPosition(Vector3 pos) 
+    {
+        targetPosition = pos;
+        velocity = Vector3.zero;
+    }
+
 
     private void OnMouseUp()
     {
@@ -30,17 +53,37 @@ public class PlayerStone : MonoBehaviour
 
         int spacesToMove = TheDiceRoller.DiceTotal;
 
+        if (spacesToMove == 0) 
+        { 
+        return;
+        }
 
+
+        moveQueue = new Tile[spacesToMove];
         Tile finalTile = currentTile;
 
         for (int i = 0; i < spacesToMove; i++)
         {
-            if (currentTile == null)
+            if (finalTile == null)
             {
                 finalTile = StartingTile;
             }
             else {
+                if (finalTile.NextTiles == null || finalTile.NextTiles.Length == 0)
+                {
+                    Debug.Log("SCORE");
+                    Destroy(gameObject);
+                    return;
+                }
+                else if (finalTile.NextTiles.Length > 1 ) 
+                {
+                    //TODO branch based on player
+                    finalTile = finalTile.NextTiles[0];
+                }
+                else
+                {
                 finalTile = finalTile.NextTiles[0];
+                }
             }
         }
 
@@ -49,8 +92,8 @@ public class PlayerStone : MonoBehaviour
             return;
         }
 
-        this.transform.position = finalTile.transform.position + new Vector3(0, 0.6f, 0);
-
+        //this.transform.position = finalTile.transform.position + new Vector3(0, 0.6f, 0);
+        SetNewTargetPosition(finalTile.transform.position + new Vector3(0, 0.6f, 0));
         currentTile = finalTile;
 
     }
